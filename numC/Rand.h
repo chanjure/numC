228aba11fb234dbd0d4b2661d737d9f7d6a556b8
Chanju Park <chanjure@snu.ac.kr>
2022-02-17 19:29:49 +0900

    CMRG update

--- numC/Rand.h
+++ numC/Rand.h
@@ -85,7 +85,7 @@ struct LCG{
  * Example:
  * @details MRG(seed)
  *
- * Tag: LCG RNG
+ * Tag: MRG RNG
  */
 struct MRG{
 	long long a1,a2,m,Ip,Ipp,I;
@@ -108,28 +108,62 @@ struct MRG{
 /**
  * @brief Combined Multiple Recursive Generator
  *
- * This is Multiple Recursive Generator
- * 
+ * This is Combined Multiple Recursive Generator of order k=3
+ * I_{1n} = a_{11}*I_{1n-1} + a_{12}*I_{1n-2} (mod m1)
+ * I_{2n} = a_{21}*I_{2n-1} + a_{22}*I_{2n-2} (mod m2)
+ * I_n = I_{1n} - I_{2n}
+ * a_{i} and m are chosen from L'Ecuyer (1998)
  *
- * Period : 
+ * Period : ~ 2^63
  * Diehard : 
  * TestU01 :
  *
  * Parameters:
- * @param a (int) multiplier
- * @param c (int) increment
- * @param m (int) modulus
+ * @param a_{ij} (int) multipliers
+ * @param m1, m2 (int) modulus
+ * @param Ip1, Ip2 (int) I_{n-1}
+ * @parma Ipp1, Ipp2 (int) I_{n-2}
  * @param I0 (int) seed
  *
  * Returns
- * @return returns 0
+ * @return returns random number
  *
  * Example:
- * @details ftn(x,y);
+ * @details MRG(seed)
  *
- * Tag: LCG RNG
+ * Tag: CMRG RNG
  */
 struct CMRG{
+	long long a11,a12,a21,a22,m1,m2,Ip1,Ip2,Ipp1,Ipp2,I1,I2;
+	
+	CMRG(long long I0) : a11(3308108773LL), a12(6149300867LL), a21(3262668826LL), a22(7914571809LL), m1(LLONG_MAX - 21129), m2(LLONG_MAX - 275025) {
+		// First component
+		Ipp1 = (a12*I0) % m1;
+		Ip1 = (a11*Ipp1 - a12*I0) % m1;
+		
+		// Second component
+		Ipp2 = (a22*I0) % m2;
+		Ip2 = (a21*Ipp2 - a22*I0) % m2;
+	}
+	
+	inline long long randint(){
+		// First component
+		I1 = (a11*Ip1 - a12*Ipp1) % m1;
+		if (I1 < 0.0) I1 += m1;
+		Ipp1 = Ip1;
+		Ip1 = I1;
+
+		// Second component
+		I2 = (a21*Ip2 - a22*Ipp2) % m2;
+		if (I2 < 0.0) I2 += m2;
+		Ipp2 = Ip2;
+		Ip2 = I2;
+
+		// Combination
+		if (I1 <= I2) return I1 - I2 + m1;
+		else return I1 - I2;
+	}
+	inline double uniform() { return 1.0842021724855069177094666532869e-19*randint(); }
 };
 
 /**
